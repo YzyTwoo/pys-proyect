486e1044534eac680c93115ca15f0ebf14f336a2
Tiziano Orieta <tiziorieta@gmail.com>
2023-11-29 13:55:22 -0300

    pimienta y sal dinámico

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+let indexController = require('../controllers/indexController.js')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.home);
+router.get('/detalle/:id', indexController.detalle)
 
 module.exports = router;
